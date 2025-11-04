cc82bf5fa89871ab3c06e0f2c5453feab1613d56
Patricia Loayza Frykberg <patricia_lf_86@hotmail.com>
2025-11-04 20:35:10 +0100

    Added array and foreach-loops

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
+<?php
+
+require 'data.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,7 +15,30 @@
     <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
     <title>Kinforma</title>
 </head>
+
 <body>
-    
+
+    <!--Se till att fonten ni använder stödjer ø-tecknet (de flesta moderna fonter gör det)
+Om ni ska ha det på webben, kan ni använda HTML-koden: &oslash; för ø eller &Oslash; för Ø
+I CSS/text: bara kopiera in ø direkt-->
+
+    <div class="products-container">
+        <h1>Thø</h1>
+        <div class="list-box-container">
+            <?php
+            foreach ($products as $product => $value) { ?>
+                <a href="product-page.php?product=<?= $product; ?>" class="product-link">
+                    <section>
+                        <img src="<?= $value['img1']; ?>" alt="<?= $product; ?> img1" />
+                        <p><?= $product; ?></p>
+
+                    </section>
+                </a>
+            <?php } ?>
+        </div>
+    </div>
+
+
 </body>
+
 </html>
--- product-page.php
+++ product-page.php
@@ -1,3 +1,22 @@
 <?php
+require 'data.php';
 
-//Loopa igenom array i data.php för info om enskilda produkter
+$productName = $_GET['product'];
+$product = $products[$productName];
+?>
+
+<div class="product-detail">
+    <h1><?= $productName; ?></h1>
+    <img src="<?= $product['img1']; ?>" alt="<?= $productName; ?> img1" />
+
+    <p><strong>Beskrivning:</strong> <?= $product['description']; ?></p>
+
+    <p><strong>Pris:</strong> <?= $product['prize']; ?></p>
+
+    <img src="<?= $product['img2']; ?>" alt="<?= $productName; ?> img2" />
+    <img src="<?= $product['img3']; ?>" alt="<?= $productName; ?> img3" />
+    <img src="<?= $product['img4']; ?>" alt="<?= $productName; ?> img4" />
+    <img src="<?= $product['img5']; ?>" alt="<?= $productName; ?> img5" />
+</div>
+
+<!--Loopa igenom array i data.php för info om enskilda produkter
