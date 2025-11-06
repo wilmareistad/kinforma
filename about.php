bb61dde0dffee7af38d181a14fde7945aec5fa43
Wilma <wilmareistad@icloud.com>
2025-11-06 19:52:35 +0100

    addad margin and padding 0 on *

--- about.php
+++ about.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . "/header.php"
+require __DIR__ . "/header.php";
 
 ?>
 <!-- // länken till css ska tas bort när den är kopplad till index -->
@@ -10,4 +10,13 @@ require __DIR__ . "/header.php"
     <h2>VÅR HISTORIA</h2>
     <p>Upplev en ny dimension av mode med vår senaste kollektion. Designad av dig, drivet av teknik, skapat för planeten.</p>
     <img class="team" src="https://qualityoutlet.com/wp-content/uploads/2025/03/RevolutionRace.2.1.jpg" alt="bild på teamet">
+    <h2>PERSONLIG DESIGN</h2>
+    <p>Upplev en ny dimension av mode med vår senaste kollektion. Designad av dig, drivet av teknik, skapat för planeten.</p>
+    <h2>SMART TEXTIL</h2>
+    <p>Upplev en ny dimension av mode med vår senaste kollektion. Designad av dig, drivet av teknik, skapat för planeten.</p>
+    <img class="smart-textil" src="https://qualityoutlet.com/wp-content/uploads/2025/03/RevolutionRace.2.1.jpg" alt="bild på teamet">
 </main>
+
+<?php
+require __DIR__ . "/newsletter.php";
+require __DIR__ . "/footer.php";
