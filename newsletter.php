2753e78093af5e22fca1036598d307cefddb8c30
Wilma <wilmareistad@icloud.com>
2025-11-06 14:48:36 +0100

    Added email request

--- newsletter.php
+++ newsletter.php
@@ -1,1 +1,16 @@
 <?php
+if (isset($_POST['email']) && $_POST['E-post']) {
+    $name = $_POST['title'];
+    echo "Your email address is $name and your password is $password";
+}
+?>
+
+<h3>Din inbjudan</h3>
+<p>Bli en del av Kinformas community och ta del av rabatter, aktiviteter och mycket annat.</p>
+<p>E-post</p>
+
+<form action="newsletter.php" method="post">
+    <label>E-post</label>
+    <input type="email" name="title" placeholder="john.smith@gmail.com">
+    <button type="submit">Gå med</button>
+</form>
