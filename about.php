1d41c7266f16fb48dc7977d60574d6e2f73f4459
Wilma <wilmareistad@icloud.com>
2025-11-06 19:25:00 +0100

    added style to h2

--- about.php
+++ about.php
@@ -5,3 +5,4 @@ require __DIR__ . "/header.php"
 ?>
 
 <h2>VÅR HISTORIA</h2>
+<p>Upplev en ny dimension av mode med vår senaste kollektion. Designad av dig, drivet av teknik, skapat för planeten.</p>
