1415fb87ae8331d8179402c167f937373ca2491f
Wilma <wilmareistad@icloud.com>
2025-11-07 16:11:16 +0100

    added button and text

--- about.php
+++ about.php
@@ -6,16 +6,20 @@ require __DIR__ . "/header.php";
 <!-- // länken till css ska tas bort när den är kopplad till index -->
 <link rel="stylesheet" href="about.css">
 
-<main class="main-about">
+<section class="container-about">
     <h2>VÅR HISTORIA</h2>
     <p>Upplev en ny dimension av mode med vår senaste kollektion. Designad av dig, drivet av teknik, skapat för planeten.</p>
-    <img class="team" src="https://qualityoutlet.com/wp-content/uploads/2025/03/RevolutionRace.2.1.jpg" alt="bild på teamet">
+    <div>
+        <p class="klubb-kin">Klubb Kin</p>
+        <img class="team" src="https://qualityoutlet.com/wp-content/uploads/2025/03/RevolutionRace.2.1.jpg" alt="bild på teamet">
+        <button>Våra arbetsätt</button>
+    </div>
     <h2>PERSONLIG DESIGN</h2>
     <p>Upplev en ny dimension av mode med vår senaste kollektion. Designad av dig, drivet av teknik, skapat för planeten.</p>
     <h2>SMART TEXTIL</h2>
     <p>Upplev en ny dimension av mode med vår senaste kollektion. Designad av dig, drivet av teknik, skapat för planeten.</p>
     <img class="smart-textil" src="https://qualityoutlet.com/wp-content/uploads/2025/03/RevolutionRace.2.1.jpg" alt="bild på teamet">
-</main>
+</section>
 
 <?php
 require __DIR__ . "/newsletter.php";
