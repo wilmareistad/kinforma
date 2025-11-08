02008856277f1b0f1650a775a0daa093e4389b72
Patricia Loayza Frykberg <patricia_lf_86@hotmail.com>
2025-11-08 15:59:32 +0100

    fixed array & changed height to min-height

--- data.php
+++ data.php
@@ -2,15 +2,9 @@
 
 $products = [
     'JacketName' => [
-
         'img1' => 'assets/Jacka_Preview.png',
         'description' => 'Beskrivning',
         'prize' => '1000SEK',
-
-        'img1' => 'assets/Jacka.jpg',
-        'description' => 'Vattentät dunjacka',
-        'prize' => '1000kr',
-
         'img2' => 'assets/jacka2.jpg',
         'img3' => 'assets/jacka3.jpg',
         'img4' => 'assets/jacka4.jpg',
@@ -18,7 +12,6 @@ $products = [
     ],
 
     'bottleName' => [
-
         'img1' => 'assets/Bottle_Preview.png',
         'description' => 'Beskrivning',
         'prize' => '1000SEK',
@@ -46,30 +39,10 @@ $products = [
         'img3' => 'assets/',
         'img4' => 'assets/',
         'img5' => 'assets/',
-
-        'img1' => 'assets/flaska.jpg',
-        'description' => 'Primera División (Spain)',
-        'prize' => 1,
-        'img2' => 'https://upload.wikimedia.org/wikipedia/en/4/47/FC_Barcelona_%28crest%29.svg',
-        'img3' => 'https://upload.wikimedia.org/wikipedia/en/4/47/FC_Barcelona_%28crest%29.svg',
-        'img4' => 'https://upload.wikimedia.org/wikipedia/en/4/47/FC_Barcelona_%28crest%29.svg',
-        'img5' => 'https://upload.wikimedia.org/wikipedia/en/4/47/FC_Barcelona_%28crest%29.svg',
-    ],
-
-    'sunscreenName' => [
-        'img1' => 'assets/solkräm.jpg',
-        'description' => 'Primera División (Spain)',
-        'prize' => 1,
-        'img2' => 'https://upload.wikimedia.org/wikipedia/en/4/47/FC_Barcelona_%28crest%29.svg',
-        'img3' => 'https://upload.wikimedia.org/wikipedia/en/4/47/FC_Barcelona_%28crest%29.svg',
-        'img4' => 'https://upload.wikimedia.org/wikipedia/en/4/47/FC_Barcelona_%28crest%29.svg',
-        'img5' => 'https://upload.wikimedia.org/wikipedia/en/4/47/FC_Barcelona_%28crest%29.svg',
-
     ],
 
     'keychainName' => [
         'img1' => 'assets/nyckelring.jpg',
-
         'description' => 'Beskrivning',
         'prize' => '1000SEK',
         'img2' => 'assets/',
@@ -86,24 +59,6 @@ $products = [
         'img3' => 'assets/',
         'img4' => 'assets/',
         'img5' => 'assets/',
-
-        'description' => 'Primera División (Spain)',
-        'prize' => 1,
-        'img2' => 'https://upload.wikimedia.org/wikipedia/en/4/47/FC_Barcelona_%28crest%29.svg',
-        'img3' => 'https://upload.wikimedia.org/wikipedia/en/4/47/FC_Barcelona_%28crest%29.svg',
-        'img4' => 'https://upload.wikimedia.org/wikipedia/en/4/47/FC_Barcelona_%28crest%29.svg',
-        'img5' => 'https://upload.wikimedia.org/wikipedia/en/4/47/FC_Barcelona_%28crest%29.svg',
-    ],
-
-    'bottlewarmerName' => [
-        'img1' => 'assets/värmeplatta.jpg',
-        'description' => 'Primera División (Spain)',
-        'prize' => 1,
-        'img2' => 'https://upload.wikimedia.org/wikipedia/en/4/47/FC_Barcelona_%28crest%29.svg',
-        'img3' => 'https://upload.wikimedia.org/wikipedia/en/4/47/FC_Barcelona_%28crest%29.svg',
-        'img4' => 'https://upload.wikimedia.org/wikipedia/en/4/47/FC_Barcelona_%28crest%29.svg',
-        'img5' => 'https://upload.wikimedia.org/wikipedia/en/4/47/FC_Barcelona_%28crest%29.svg',
-
     ],
 ];
 
