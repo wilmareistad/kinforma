5712d720da4dac3b037e3492166636ee583b77d8
Wilma <wilmareistad@icloud.com>
2025-11-12 17:47:32 +0100

    added new style to about

--- about.php
+++ about.php
@@ -6,22 +6,33 @@ require __DIR__ . "/header.php";
 ?>
 <!-- // länken till css ska tas bort när den är kopplad till index -->
 <link rel="stylesheet" href="about.css">
-
-<section class="container-about">
-    <h2>VÅR HISTORIA</h2>
-    <p>Upplev en ny dimension av mode med vår senaste kollektion. Designad av dig, drivet av teknik, skapat för planeten.</p>
-    <div>
-        <p class="klubb-kin">Klubb Kin</p>
-        <img class="team" src="https://qualityoutlet.com/wp-content/uploads/2025/03/RevolutionRace.2.1.jpg" alt="bild på teamet">
-        <button>Våra arbetsätt</button>
+<main class="about-main">
+    <section class="container-about">
+        <div class="about-hero">
+            <p class="klubb-kin">Våra arbetsätt</p>
+            <img class="team" src="https://qualityoutlet.com/wp-content/uploads/2025/03/RevolutionRace.2.1.jpg" alt="bild på teamet">
+            <!-- <button>Våra arbetsätt</button> -->
+        </div>
+    </section>
+    <section class="about-info">
+        <div class="persenol-design">
+            <h3>PERSONLIG DESIGN</h3>
+            <p>Upplev en ny dimension av mode med vår senaste kollektion. Designad av dig, drivet av teknik, skapat för planeten.</p>
+        </div>
+        <div class="sustainable-fashion">
+            <p>Upplev en ny dimension av mode med vår senaste kollektion. Designad av dig, drivet av teknik, skapat för planeten.</p>
+            <h3>HÅLLBART MODE</h3>
+        </div>
+        <div class="smart-textil">
+            <h3>SMART TEXTIL</h3>
+            <p>Upplev en ny dimension av mode med vår senaste kollektion. Designad av dig, drivet av teknik, skapat för planeten.</p>
+        </div>
+    </section>
+    <div class="smart-textil-container">
+        <img src="https://qualityoutlet.com/wp-content/uploads/2025/03/RevolutionRace.2.1.jpg" alt="bild på teamet">
+        <img src="https://qualityoutlet.com/wp-content/uploads/2025/03/RevolutionRace.2.1.jpg" alt="bild på teamet">
     </div>
-    <h2>PERSONLIG DESIGN</h2>
-    <p>Upplev en ny dimension av mode med vår senaste kollektion. Designad av dig, drivet av teknik, skapat för planeten.</p>
-    <h2>SMART TEXTIL</h2>
-    <p>Upplev en ny dimension av mode med vår senaste kollektion. Designad av dig, drivet av teknik, skapat för planeten.</p>
-    <img class="smart-textil" src="https://qualityoutlet.com/wp-content/uploads/2025/03/RevolutionRace.2.1.jpg" alt="bild på teamet">
-</section>
-
+</main>
 <?php
 require __DIR__ . "/newsletter.php";
 require __DIR__ . "/footer.php";
