13836453f88524ef2627df61117e4723127bb789
Wilma <wilmareistad@icloud.com>
2025-11-19 13:59:20 +0100

    added test.php for colors on productpage

---
+++ test.php
@@ -0,0 +1,26 @@
+<link rel="stylesheet" href="test.css">
+
+<section class="color-choice">
+    <div class="colors">
+        <p>Färg</p>
+        <div class="color-buttons">
+            <button>Signal</button>
+            <button>Ormbunke</button>
+            <button>Salvia</button>
+        </div>
+    </div>
+    <div class="guide">
+        <div class="guide-size">
+            <p>Storlek</p>
+            <a>Guide</a>
+        </div>
+        <div class="size-button">
+            <button>XS</button>
+            <button>S</button>
+            <button>M</button>
+            <button>L</button>
+            <button>XL</button>
+        </div>
+    </div>
+    <button class="varukorg">Lägg i varukorgen</button>
+</section>
