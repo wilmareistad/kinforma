5ff96f98568dd76ee340244c748ecc96052dd832
Patricia Loayza Frykberg <patricia_lf_86@hotmail.com>
2025-11-19 13:51:25 +0100

    Updated Product-img carousel

--- product-page.php
+++ product-page.php
@@ -5,46 +5,22 @@ require __DIR__ . "/data.php";
 
 // 1️⃣ Kontrollera att "product" finns i URL
 ?>
+
+<?php
+$productName = $_GET['product'];
+$product = $products[$productName];
+?>
+
 <section class="product-page-hero">
-    <div class="carousel">
-
-        <div class="carousel-images">
-            <img src="/assets/Karbinhake_Orange_Framifran_9_10.png" alt="Bild 1" class="carousel-slide active">
-            <img src="/assets/Karbinhake_Orange_Framifran-Oppen_Hake_9_10.png" alt="Bild 2" class="carousel-slide">
-            <img src="/assets/Close_Up_Logga_Test_2.png" alt="Bild 3" class="carousel-slide">
-            <img src="/assets/Close_Up_Logga_Test_3.png" alt="Bild 4" class="carousel-slide">
-            <img src="/assets/Close_Up_Logga_Test_3.png" alt="Bild 5" class="carousel-slide">
-            <img src="/assets/Close_Up_Logga_Test_3.png" alt="Bild 6" class="carousel-slide">
-        </div>
-
-        <!-- Prickar för att visa vilken bild (valfritt) -->
-        <div class="carousel-dots">
-            <span class="dot active" onclick="goToSlide(0)"></span>
-            <span class="dot" onclick="goToSlide(1)"></span>
-            <span class="dot" onclick="goToSlide(2)"></span>
-            <span class="dot" onclick="goToSlide(3)"></span>
-            <span class="dot" onclick="goToSlide(4)"></span>
-            <span class="dot" onclick="goToSlide(5)"></span>
-        </div>
-    </div>
-</section>
-
-<section class="info-product">
-
-    <?php
-    $productName = $_GET['product'];
-    $product = $products[$productName];
-    ?>
-
     <div class="product-cont">
-        <!--<section class="product-images">
+        <section class="product-images">
             <img src="<?= $product['img1']; ?>" alt="<?= $productName; ?> img1" />
             <img src="<?= $product['img2']; ?>" alt="<?= $productName; ?> img2" />
             <img src="<?= $product['img3']; ?>" alt="<?= $productName; ?> img3" />
             <img src="<?= $product['img4']; ?>" alt="<?= $productName; ?> img4" />
             <img src="<?= $product['img5']; ?>" alt="<?= $productName; ?> img5" />
             <img src="<?= $product['img6']; ?>" alt="<?= $productName; ?> img6" />
-        </section>-->
+        </section>
 
         <section class="product-detail">
             <h2><?= $productName; ?></h2>
@@ -57,7 +33,7 @@ require __DIR__ . "/data.php";
 <section class="THO">
     <div class="products-container">
         <div class="THO-explore">
-            <h3>Utforska <span class="italic">Th&oslash</span> kollektionen</h3>
+            <h3>Komplettera</h3>
             <a href="collection.php"><img src="/assets/Pil.svg"></a>
         </div>
 
